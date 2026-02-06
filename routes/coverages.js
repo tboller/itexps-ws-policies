284deee25a00a76f73caa43a1daa25eae34965b2
irinaclp-lgtm <IrinaCLP@gmail.com>
2026-02-06 16:53:24 -0600

    Added coverage routes

--- routes/coverages.js
+++ routes/coverages.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const helper = require('../helper');
-const policies = require('../services/policies');
-const validation = require('../validation/policies');
+const coverages = require('../services/coverages');
+const validation = require('../validation/coverages');
 
-/* GET policies */
+/* GET coverages */
 router.get('/', async (req, res, next) => {
   try {
     const error = validation.validateQuery(req.query);
@@ -12,58 +12,58 @@ router.get('/', async (req, res, next) => {
       throw helper.apiError(400, error, req);
     }
 
-    res.json(await policies.getMultiplePolicies(req.query, req.query.page));
+    res.json(await coverages.getMultipleCoverages(req.query, req.query.page));
   } catch (err) {
     next(err);
   }
 });
 
-/* GET policy by ID */
+/* GET Coverage by ID */
 router.get('/:id', async (req, res, next) => {
   try {
-    const policy = await policies.getById(req.params.id);
-    if (!policy) {
-      throw helper.apiError(404, 'Policy not found', req);
+    const coverage = await coverages.getById(req.params.id);
+    if (!coverage) {
+      throw helper.apiError(404, 'Coverage not found', req);
     }
-    res.json(policy);
+    res.json(coverage);
   } catch (err) {
     next(err);
   }
 });
 
-/* POST policies */
+/* POST coverages */
 router.post('/', async (req, res, next) => {
   try {
-    const error = validation.validateCreatePolicy(req.body);
+    const error = validation.validateCreateCoverage(req.body);
     if (error) {
       throw helper.apiError(400, error, req);
     }
 
-    const result = await policies.create(req.body);
+    const result = await coverages.create(req.body);
     res.status(201).json(result);
   } catch (err) {
     next(err);
   }
 });
 
-/* PUT policies */
+/* PUT coverages */
 router.put('/:id', async (req, res, next) => {
   try {
-    const error = validation.validateUpdatePolicy(req.body);
+    const error = validation.validateUpdateCoverage(req.body);
     if (error) {
       throw helper.apiError(400, error, req);
     }
 
-    res.json(await policies.update(req.params.id, req.body));
+    res.json(await coverages.update(req.params.id, req.body));
   } catch (err) {
     next(err);
   }
 });
 
-/* DELETE policies */
+/* DELETE coverages */
 router.delete('/:id', async (req, res, next) => {
   try {
-    await policies.remove(req.params.id);
+    await coverages.remove(req.params.id);
     res.status(204).send();
   } catch (err) {
     next(err);
